5c7e5b20d9fcf4e58f5af003b958e21b2ba82eb6
Zamiatina Ekaterina <k228747@yandex.ru>
2019-06-30 19:13:07 +0300

    fix name bug

--- src/components/Form/FormName.js
+++ src/components/Form/FormName.js
@@ -9,28 +9,29 @@ class FormEmail extends FormInput {
     }
   }
 
-    // считаем что имя заполнено верно, если в нем больше 2 букв
-    static isCorrect = (str) => {
-      return str.length > 2
-    };
+  // считаем что имя заполнено верно, если в нем больше 2 букв
+  static isCorrect = (str) => {
+    str = str.replace(/\s+/g, '')
+    return str.length > 2
+  };
 
-    render () {
-      return (
-        <p className='form__item'>
-          <input type="text"
-            className={`form__input ${this.props.value ? '' : 'form__input_empty '}` +
+  render () {
+    return (
+      <p className='form__item'>
+        <input type="text"
+          className={`form__input ${this.props.value ? '' : 'form__input_empty '}` +
                        `${FormEmail.isCorrect(this.props.value) ? '' : 'form__input_incorrect'}`}
-            name='name'
-            id='name'
-            onChange={this.clickHandler('[^а-яё ]')}
-            value={this.props.value}
-            onFocus={() => this.setState({ label: 'Имя на русском языке, больше 2 букв *' })}
-            onBlur={() => this.setState({ label: 'Имя*' })}
-          />
-          <label className='required form__label' htmlFor='name'>{this.state.label}</label>
-        </p>
-      )
-    }
+          name='name'
+          id='name'
+          onChange={this.clickHandler('[^а-яё ]')}
+          value={this.props.value}
+          onFocus={() => this.setState({ label: 'Имя на русском языке, больше 2 букв *' })}
+          onBlur={() => this.setState({ label: 'Имя*' })}
+        />
+        <label className='required form__label' htmlFor='name'>{this.state.label}</label>
+      </p>
+    )
+  }
 }
 
 export default FormEmail
